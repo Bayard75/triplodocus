1c38758b76c87f2b20c28accecef9e44188ac999
Bayard75 <bayard.maniraho@gmail.com>
2020-06-20 19:00:09 +0200

    version mobile ajouté

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,7 @@ from flask import render_template, request
 @app.route('/')
 @app.route('/acceuil')
 def acceuil():
+    user_agent = request.headers.get('User-Agent')
+    if 'Mobi' in user_agent:
+        return render_template('acceuil_mobile.html')
     return render_template('acceuil.html')
